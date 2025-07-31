9ebf8d582373f40cf868d62a422c62e9d3cc2faf
FrancescoCellitti <francescocellitti@virgilio.it>
2025-07-31 16:31:30 +0200

    call to one films with reviews work

--- server.js
+++ server.js
@@ -1,27 +1,63 @@
-require ('dotenv').config()
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const Port = process.env.PORT
 const connection = require('./data/connection')
 
-app.listen(Port, ()=>{
+app.listen(Port, () => {
     console.log(`server is now running on http://localhost:${Port}`)
 })
 
 app.get('/', (req, res) => {
 
-  res.send('Welcome to films express server! The server is running')
+    res.send('Welcome to films express server! The server is running')
 
 })
 
-app.get('/api/films', (req, res)=>{
+app.get('/api/films', (req, res) => {
     const sql = 'select * from movies'
 
-    connection.query(sql, (err, results)=>{
-        if(err) return res.status(500).json({
+    connection.query(sql, (err, results) => {
+        if (err) return res.status(500).json({
             error: true,
             message: err.message
         })
+        console.log(results)
+        res.json(results)
+    })
+})
+
+
+app.get('/api/films/:id', (req, res) => {
+    const filmId = req.params.id
+
+    const sql = `
+        SELECT 
+            movies.title,
+            reviews.name,
+            reviews.vote,
+            reviews.text,
+            reviews.created_at,
+            reviews.updated_at
+           
+        FROM movies
+        LEFT JOIN reviews  ON movies.id = reviews.movie_id
+        WHERE movies.id = ?
+    `
+
+    connection.query(sql, [filmId], (err, results) => {
+        if (err) return res.status(500).json({
+            error: true,
+            message: err.message
+        })
+
+        if (results.length === 0) {
+            return res.status(404).json({
+                error: true,
+                message: 'Film non trovato'
+            })
+        }
+
         console.log(results)
         res.json(results)
     })
